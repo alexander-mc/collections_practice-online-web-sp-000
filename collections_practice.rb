b78269a761f2a37fe2a7f3281408b75fe19997c9
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-21 04:18:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,5 +2,9 @@ def sort_array_asc(array)
   array.sort
 end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
 array = [1,4,2]
-puts sort_array_asc(array).inspect
+puts sort_array_asc(desc).inspect
