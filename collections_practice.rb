567325a313ed59505c6c71e658ec3aa019e5c9e7
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-21 04:20:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,8 +3,14 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
+  array.sort{||}
+end
+
+=begin # OR USE THE METHOD BELOW
+def sort_array_desc(array)
   array.sort.reverse
 end
+=end
 
 array = [1,4,2]
 puts sort_array_desc(array).inspect
