ab1016dc8015ab954c70adac4335936150f5b026
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-21 05:07:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,9 +17,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  temp_2 = array[2]
-  array[2] = array[1]
-  array[1] = temp_2
+  array[1], array[2] = array[2], array[1]
   return array
 end
 
